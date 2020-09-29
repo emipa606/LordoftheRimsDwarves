314b8d7ebe3652c6d460e7bd2b24f481c0427405
emipa606 <emil.offermo@litet.net>
2020-09-29 22:31:13 +0200

    First publish

---
+++ Source/Dwarves/DwarfMod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Dwarves
+{
+    public class DwarfMod : Mod
+    {
+        
+        public DwarfMod(ModContentPack content) : base(content)
+        {
+        }
+        
+    }
+}
